1129689b2b363cee89d23739ec7b0776443221fe
student10631566-ux <student10631566@gmail.com>
2025-12-14 19:36:06 +0530

    Update root GET route to return HTML content for the Client Information System

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Single GET route returning plain text
+// Single GET route returning HTML
 app.get('/', (req, res) => {
-  res.send('App running');
+  res.send(`
+    <html>
+      <head>
+        <title>Client Information System</title>
+      </head>
+      <body>
+        <h1>Client Information System</h1>
+        <p>Welcome to the Client Information System. This system helps manage and organize client data efficiently.</p>
+      </body>
+    </html>
+  `);
 });
 
 // Start the server
